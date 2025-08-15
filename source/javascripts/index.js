f9f207efdb0d0e692bbc3961018c9ec088247130
Sunjun Hwang <sunjun.software@gmail.com>
2025-08-15 11:37:42 +0900

    removed password

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -25,60 +25,6 @@
   };
 })();
 
-const secret_section = document.getElementById("secret");
-const password_box = document.querySelector("#password-box");
-// const enter_button = document.getElementById("enter-button");
-const enter_button = document.querySelector("#forbidden-button");
-const message = document.querySelector("#message");
-
-const password1 = "roast";
-const password2 = "bread";
-var question = 1;
-var count = 10;
-var passed = false;
-
-enter_button.addEventListener("click", (event) => {
-  event.preventDefault();
-  // console.log(password_box.value);
-
-  if(!passed){
-    if(question === 1){
-      if(password_box.value === password1 && count >= 1){
-        message.innerText = `Enter the password ${count} more times.\n`;
-        password_box.value = "";
-        count = count - 1;
-
-        if(count === 4){
-          message.classList.add("red");
-        }
-
-      } else if (password_box.value === password1 && count === 0){
-        question = 2;
-        message.innerText = "What do you put in a toaster?\n";
-        password_box.value = "";
-      } else {
-        message.innerText = `Wrong password`;
-        password_box.value = "";
-      }
-    } else if (question === 2){
-      if(password_box.value === password2){
-        message.classList.remove("red");
-        message.innerText = "Congratulations! You have unlocked the forbidden section.";
-        passed = true;
-        password_box.classList.add("d-none");
-        enter_button.classList.add("d-none");
-        secret_section.classList.remove("d-none");
-        setTimeout(() => {
-          secret_section.classList.remove("tab-hidden");
-        }, 100);
-      } else{
-        message.innerText = "What do you put in a toaster?\nWrong answer. Try again.";
-        password_box.value = "";
-      }
-    }
-  }
-
-});
 
 const tab_sections = document.getElementsByClassName("tab-section");
 const tabs = document.getElementsByClassName("tab-underlined");
